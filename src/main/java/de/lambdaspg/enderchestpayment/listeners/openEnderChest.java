6eee5346549129be2ebff3a0406fc733d49733a1
LambdaSpg <LambdaSpg@protonmail.com>
2021-01-20 09:32:32 +0100

    Enderchests only take money, if a Netherrite block is below them

--- src/main/java/de/lambdaspg/enderchestpayment/listeners/openEnderChest.java
+++ src/main/java/de/lambdaspg/enderchestpayment/listeners/openEnderChest.java
@@ -3,7 +3,10 @@ package de.lambdaspg.enderchestpayment.listeners;
 import de.lambdaspg.enderchestpayment.EnderChestPayment;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,15 +24,20 @@ public class openEnderChest implements Listener {
     public void onOpen(PlayerInteractEvent e){
         Economy econ = EnderChestPayment.getEconomy();
         Player p = e.getPlayer();
+        World world = p.getWorld();
+        Block clickedBlock = e.getClickedBlock();
 
         if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
             if(e.getClickedBlock().getType().equals(Material.ENDER_CHEST)){
-                if(econ.getBalance(e.getPlayer()) >= plugin.getConfig().getDouble("Enderchest.cost")){
-                    econ.withdrawPlayer(e.getPlayer(), plugin.getConfig().getDouble("Enderchest.cost"));
-                    p.sendMessage(ChatColor.AQUA + "$" +String.valueOf(plugin.getConfig().getDouble("Enderchest.cost")) +" have been withdrawn from your bank account");
-                }else {
-                    p.sendMessage(ChatColor.RED + "You do not have enough money to open your Enderchest");
-                    e.setCancelled(true);
+                Block loc = world.getBlockAt(e.getClickedBlock().getX(), clickedBlock.getY() - 1, clickedBlock.getZ());
+                if(loc.getType().equals(Material.NETHERITE_BLOCK)) {
+                    if (econ.getBalance(e.getPlayer()) >= plugin.getConfig().getDouble("Enderchest.cost")) {
+                        econ.withdrawPlayer(e.getPlayer(), plugin.getConfig().getDouble("Enderchest.cost"));
+                        p.sendMessage(ChatColor.AQUA + "$" + String.valueOf(plugin.getConfig().getDouble("Enderchest.cost")) + " have been withdrawn from your bank account");
+                    } else {
+                        p.sendMessage(ChatColor.RED + "You do not have enough money to open your Enderchest");
+                        e.setCancelled(true);
+                    }
                 }
             }
         }
